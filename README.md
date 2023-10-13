78d41f9eaa3f88345f7a7ac4e0801c8a7081d33a
Tuomo Kriikkula <tuokri@tuta.io>
2023-10-13 19:05:25 +0300

    Code generation updates

--- README.md
+++ README.md
@@ -7,12 +7,11 @@ where the messages take as few bytes as possible.
 ## Dependencies
 
 - [Inja](https://github.com/pantor/inja)
-- [JSON for Modern C++ (nlohmann/json)](https://github.com/nlohmann/json)
-- [Boost (Program Options)](https://www.boost.org/doc/libs/1_83_0/doc/html/program_options.html)
+- [Boost (Program Options, Filesystem)](https://www.boost.org/)
     - Tested with Boost versions 1.82 and 1.83.
 
 Install using vcpkg:
 
 ```shell
-vcpkg install inja nlohmann-json boost-program-options
+vcpkg install inja boost-program-options boost-filesystem
 ```
--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,24 @@
 #include <vector>
 
 #include <inja/inja.hpp>
-#include <nlohmann/json.hpp>
+
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
 #include <boost/program_options.hpp>
 
+namespace fs = boost::filesystem;
 namespace po = boost::program_options;
 
-static constexpr size_t header_size = 3;
+static constexpr auto g_template_dir = "templates";
+static constexpr auto g_cpp_template = "cpp.jinja";
+static constexpr auto g_uscript_template = "uscript.jinja";
+static constexpr auto g_uscript_message_handler_template = "uscript_message_handler.jinja";
 
-static std::unordered_map<std::string, size_t> static_types{
+// Packet header size in bytes.
+static constexpr size_t g_header_size = 3;
+
+// All types with known static sizes in bytes.
+static const std::unordered_map<std::string, size_t> g_static_types{
     {"byte",  1},
     {"int",   4},
     {"float", 4},
@@ -21,17 +31,25 @@ struct MsgAnalysisResult
 {
     friend std::ostream& operator<<(std::ostream& os, const MsgAnalysisResult& result);
 
+    // Message name as declared in the input data.
     std::string name{};
+    // True if message static_size value was calculated. False for messages
+    // with dynamically sized fields or unknown size fields.
     bool has_static_size{false};
+    // Message's calculated static size. Sum of header size + static field sizes.
     size_t static_size{0};
+    // Message has static size and is always guaranteed to fit in a single packet.
     bool always_single_part{false};
 };
 
 std::ostream& operator<<(std::ostream& os, const MsgAnalysisResult& result)
 {
-    os << "{" << " name: " << result.name << ", has_static_size: " << result.has_static_size
-       << ", static_size: " << result.static_size << ", always_single_part: "
-       << result.always_single_part << " }";
+    os << "{"
+       << " name: " << result.name
+       << ", has_static_size: " << result.has_static_size
+       << ", static_size: " << result.static_size
+       << ", always_single_part: " << result.always_single_part
+       << " }";
     return os;
 }
 
@@ -51,15 +69,15 @@ MsgAnalysisResult analyze_message(const inja::json& data)
 
     result.has_static_size = std::all_of(types.cbegin(), types.cend(), [](const std::string& type)
     {
-        return static_types.contains(type);
+        return g_static_types.contains(type);
     });
 
     if (result.has_static_size)
     {
-        result.static_size = header_size;
+        result.static_size = g_header_size;
         for (const auto& type: types)
         {
-            result.static_size += static_types.at(type);
+            result.static_size += g_static_types.at(type);
         }
     }
 
@@ -71,18 +89,63 @@ MsgAnalysisResult analyze_message(const inja::json& data)
     return result;
 }
 
-void render_uscript(inja::Environment& env, const std::string& file, const inja::json& data,
-                    const std::string& uscript_out_dir = "")
+static constexpr auto capitalize = [](inja::Arguments& args)
 {
-    std::cout << env.render_file(file, data) << "\n";
+    auto str = args.at(0)->get<std::string>();
+    str[0] = static_cast<char>(std::toupper(str[0]));
+    return str;
+};
+
+// Calculates padding for index (arg 0) up to
+// max-padding (arg 1). Can be used to align columns e.g.:
+// Bytes[  0] // index = 0
+// Bytes[  1] // index = 1
+// ...
+// Bytes[ 10]
+// ...
+// Bytes[101]
+static constexpr auto pad = [](inja::Arguments& args)
+{
+    const auto index = args.at(0)->get<size_t>();
+    const auto size = args.at(1)->get<size_t>();
+    std::string ret;
+
+    if (size < 10)
+    {
+        return ret;
+    }
+
+    const auto max_pad = std::to_string(size).size();
+    const auto pad_count = max_pad - std::to_string(index).size();
+
+    std::stringstream ss;
+    for (int i = 0; i < pad_count; ++i)
+    {
+        ss << " ";
+    }
+    ret = ss.str();
+    return ret;
+};
+
+void render_uscript(inja::Environment& env, const std::string& file, const inja::json& data,
+                    const std::string& uscript_out_dir)
+{
+    const auto r = env.render_file(file, data);
+    std::cout << r << "\n";
+    fs::path out_filename = fs::path(file).filename().replace_extension(".uc");
+    fs::path out_file = fs::path{uscript_out_dir} / out_filename;
+    std::cout << "writing '" << out_file.string() << "'\n";
+    fs::ofstream out{out_file};
+    out << r << std::endl;
 }
 
 void render_cpp(inja::Environment& env, const std::string& file, const inja::json& data,
-                const std::string& cpp_out_dir = "")
+                const std::string& cpp_out_dir)
 {
 }
 
-void render(const std::string& file)
+void
+render(const std::string& file, const std::string& uscript_out_dir, const std::string& cpp_out_dir)
 {
     inja::Environment env;
 
@@ -90,38 +153,10 @@ void render(const std::string& file)
     env.set_lstrip_blocks(true);
 
     // TODO: add option to enable/disable this.
-    env.add_callback("capitalize", 1, [](inja::Arguments& args)
-    {
-        auto str = args.at(0)->get<std::string>();
-        str[0] = static_cast<char>(std::toupper(str[0]));
-        return str;
-    });
+    env.add_callback("capitalize", 1, capitalize);
+    env.add_callback("pad", 2, pad);
 
-    env.add_callback("pad", 2, [](inja::Arguments& args)
-    {
-        const auto index = args.at(0)->get<size_t>();
-        const auto size = args.at(1)->get<size_t>();
-        std::string ret;
-
-        if (size < 10)
-        {
-            return ret;
-        }
-
-        const auto max_pad = std::to_string(size).size();
-        const auto pad_count = max_pad - std::to_string(index).size();
-
-        std::stringstream ss;
-        for (int i = 0; i < pad_count; ++i)
-        {
-            ss << " ";
-        }
-        ret = ss.str();
-        return ret;
-    });
-
-    auto data = env.load_json(
-        R"(C:\Users\tkrii\Documents\My Games\Rising Storm 2\ROGame\Src\VOIPTest\Messages.json)");
+    auto data = env.load_json(file);
     data["class_name"] = file;
 
     auto& messages = data["messages"];
@@ -142,17 +177,29 @@ void render(const std::string& file)
         data["uscript_message_handler_class"] = "MessageHandler";
     }
 
+    fs::path template_dir{g_template_dir};
+    fs::path us_template = template_dir / g_uscript_template;
+    fs::path cpp_template = template_dir / g_cpp_template;
+
     std::cout << "rendering '" << file << "'\n";
-    render_uscript(env, file, data);
-    render_cpp(env, file, data);
+    render_uscript(env, us_template.string(), data, uscript_out_dir);
+    render_cpp(env, cpp_template.string(), data, cpp_out_dir);
 }
 
 int main(int argc, char* argv[])
 {
     try
     {
+        const auto default_out_dir = fs::current_path().parent_path();
+
         po::options_description desc("Options");
         desc.add_options()("help,h", "print the help message");
+        desc.add_options()("uscript-out,u",
+                           po::value<std::string>()->default_value(default_out_dir.string()),
+                           "UnrealScript code generation output directory");
+        desc.add_options()("cpp-out,c",
+                           po::value<std::string>()->default_value(default_out_dir.string()),
+                           "C++ code generation output directory");
         desc.add_options()("input-file",
                            po::value<std::vector<std::string>>()->required(), "input file(s)");
 
@@ -177,9 +224,12 @@ int main(int argc, char* argv[])
             return EXIT_FAILURE;
         }
 
+        const auto uscript_out_dir = vm["uscript-out"].as<std::string>();
+        const auto cpp_out_dir = vm["cpp-out"].as<std::string>();
+
         for (const auto& file: vm["input-file"].as<std::vector<std::string>>())
         {
-            render(file);
+            render(file, uscript_out_dir, cpp_out_dir);
         }
     }
     catch (const std::exception& ex)
@@ -190,6 +240,7 @@ int main(int argc, char* argv[])
     catch (...)
     {
         std::cout << "unknown error" << std::endl;
+        return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
