3db9f53b31b3226ffd5e47087748b39536dfe032
Tuomo Kriikkula <tuokri@tuta.io>
2023-10-16 00:46:54 +0300

    First proper test setup

--- tests/test_coding.cpp
+++ tests/test_coding.cpp
@@ -4,6 +4,13 @@
 
 #include "umb/umb.hpp"
 
+#include "TestMessages.umb.hpp"
+
 TEST_CASE("encode decode empty message")
 {
+    testmessages::umb::GetSomeStuff msg1;
+    const auto bytes = msg1.to_bytes();
+    testmessages::umb::GetSomeStuff msg2;
+    const auto ok = msg2.from_bytes(bytes);
+    CHECK(ok);
 }
