875de0c837ef7a933fbf702f545a25c52dc1951a
Tuomo Kriikkula <tuokri@tuta.io>
2023-10-24 19:21:21 +0300

    Add Valgrind helper script

---
+++ tests/gen_test_messages_with_valgrind.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Run UMB message generator with Valgrind on the test message set.
+# NOTE: on WSL Debian:
+# - fails with Valgrind 3.19.0
+# - works with Valgrind 3.21.0
+
+set +x
+set -e
+shopt -s nullglob
+
+# TODO: modularize this.
+# TODO: Valgrind runs for generator failure branches. (Argument handling, invalid files, etc.)
+
+[[ -z "${UMB_VALGRIND_BIN}" ]] && VALGRIND='valgrind' || VALGRIND="${UMB_VALGRIND_BIN}"
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+UM_GEN_EXECUTABLE="${SCRIPT_DIR}/../cmake-build-debug-wsl/uscript_msgbuf_generator"
+DATA_FILES=("${SCRIPT_DIR}"/data/*.json)
+OUT_DIR="${SCRIPT_DIR}"/out/valgrind
+
+mkdir -p "${OUT_DIR}"
+
+echo "using valgrind: '${VALGRIND}'"
+
+${VALGRIND} --leak-check=full --track-origins=yes --error-exitcode=1 \
+  "${UM_GEN_EXECUTABLE}" "${DATA_FILES[@]}" \
+  --cpp-out="${OUT_DIR}" \
+  --uscript-out="${OUT_DIR}"
