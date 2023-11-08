4ad119aba9a31d87ed531509a8b76bf0af75b639
Tuomo Kriikkula <tuokri@tuta.io>
2023-11-08 20:10:05 +0200

    Add udk_configparser requirement

--- tests/UDKTests/setup_udk_build_env.py
+++ tests/UDKTests/setup_udk_build_env.py
@@ -12,6 +12,9 @@
 # You should have received a copy of the GNU Lesser General Public License
 #     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+# TODO: rename this script.
+# TODO: leverage pytest?
+
 import asyncio
 import enum
 import glob
