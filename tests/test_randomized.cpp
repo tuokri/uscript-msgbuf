90bab6dd3591e54c14fd9483a437f419fad19990
Tuomo Kriikkula <tuokri@tuta.io>
2023-11-08 19:06:13 +0200

    Add license notice

--- tests/test_randomized.cpp
+++ tests/test_randomized.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2023  Tuomo Kriikkula
+ * This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef __JETBRAINS_IDE__
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #define DOCTEST_CONFIG_SUPER_FAST_ASSERTS
@@ -54,6 +70,7 @@ constexpr std::u16string get_rand_str(std::integer_sequence<IndexType, Is...> sl
         constexpr auto rnd_idx = get_rand<Index, FIndex, Round, i64>();
         str_in.append(1, static_cast<char16_t>(rnd_idx & 0xffff));
     });
+
     return str_in;
 }
 
