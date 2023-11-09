643d7ca7c040fd2eb7daa1ff68fe159f9e3c184a
Tuomo Kriikkula <tuokri@tuta.io>
2023-11-09 19:14:57 +0200

    Fix variable name in Valgrind script

--- tests/gen_test_messages_with_valgrind.sh
+++ tests/gen_test_messages_with_valgrind.sh
@@ -15,7 +15,7 @@ shopt -s nullglob
 UM_GEN_EXECUTABLE_DEFAULT="${SCRIPT_DIR}/../cmake-build-debug-wsl/uscript_msgbuf_generator"
 
 [[ -z "${UMB_VALGRIND_BIN}" ]] && VALGRIND='valgrind' || VALGRIND="${UMB_VALGRIND_BIN}"
-[[ -z "${UMB_GEN_BIN}" ]] && UMB_GEN_BIN=${UM_GEN_EXECUTABLE_DEFAULT} || VALGRIND="${UMB_GEN_BIN}"
+[[ -z "${UMB_GEN_BIN}" ]] && UMB_GEN=${UM_GEN_EXECUTABLE_DEFAULT} || UMB_GEN="${UMB_GEN_BIN}"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DATA_FILES=("${SCRIPT_DIR}"/data/*.json)
@@ -27,6 +27,6 @@ echo "using valgrind: '${VALGRIND}'"
 echo "using UMB generator: '${UMB_GEN_BIN}'"
 
 ${VALGRIND} --leak-check=full --track-origins=yes --error-exitcode=1 \
-  "${UMB_GEN_BIN}" "${DATA_FILES[@]}" \
+  "${UMB_GEN}" "${DATA_FILES[@]}" \
   --cpp-out="${OUT_DIR}" \
   --uscript-out="${OUT_DIR}"
