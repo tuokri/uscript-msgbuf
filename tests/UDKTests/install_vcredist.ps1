60a4b606a42d5311929dbf76658a13c385e80e59
Tuomo Kriikkula <tuokri@tuta.io>
2023-11-13 14:29:52 +0200

    Change VC Redist version from 2010 to 2012

--- tests/UDKTests/install_vcredist.ps1
+++ tests/UDKTests/install_vcredist.ps1
@@ -1,5 +1,5 @@
 Install-Module -Name VcRedist -Force
 New-Item -Path C:\Temp\VcRedist -ItemType Directory
-$VcList = Get-VcList -Release "2010"
+$VcList = Get-VcList -Release "2012"
 Save-VcRedist -VcList $VcList -Path C:\Temp\VcRedist
 Install-VcRedist -Path C:\Temp\VcRedist -VcList $VcList -Silent
