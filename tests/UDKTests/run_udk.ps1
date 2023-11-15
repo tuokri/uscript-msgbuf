8079bdb50f9cc319531551095a4e4da34e0e8cc6
Tuomo Kriikkula <tuokri@tuta.io>
2023-11-15 14:20:55 +0200

    Use Format-Table -Wrap on Get-WinEvent output

--- tests/UDKTests/run_udk.ps1
+++ tests/UDKTests/run_udk.ps1
@@ -1,5 +1,7 @@
-Start-Process -NoNewWindow -FilePath $PSScriptRoot\UDK-Lite\Binaries\Win64\UDK.com -ArgumentList "make", "-useunpublished"
+Start-Process -NoNewWindow -FilePath $PSScriptRoot\UDK-Lite\Binaries\Win64\UDK.com `
+    -ArgumentList "make", "-useunpublished"
 
 # TODO: this can list all event logs for "UDK".
 Get-WinEvent -ListLog * |
-        ForEach-Object -parallel { Get-WinEvent @{ logname = $_.logname } -ea 0 } | Where-Object message -Match UDK
+        ForEach-Object -parallel { Get-WinEvent @{ logname = $_.logname } -ea 0 } `
+            | Where-Object message -Match UDK | Format-Table -Wrap
