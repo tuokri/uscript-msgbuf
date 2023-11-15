aba08441e739ca957231be78fbef799e7ae335d4
Tuomo Kriikkula <tuokri@tuta.io>
2023-11-15 13:58:50 +0200

    Set FW arguments, print 'UDK' matches in event log

--- tests/UDKTests/allow_udk_fw.ps1
+++ tests/UDKTests/allow_udk_fw.ps1
@@ -1,5 +1,14 @@
 $UDKPath = "$PSScriptRoot\UDK-Lite\Binaries\Win64\UDK.exe"
-New-NetFirewallRule -DisplayName "UDK.exe Outbound UDP" -Direction Outbound -Program $UDKPath -Action Allow -Protocol UDP
-New-NetFirewallRule -DisplayName "UDK.exe Outbound TCP" -Direction Outbound -Program $UDKPath -Action Allow -Protocol TCP
-New-NetFirewallRule -DisplayName "UDK.exe Inbound UDP" -Direction Inbound -Program $UDKPath -Action Allow -Protocol UDP
-New-NetFirewallRule -DisplayName "UDK.exe Inbound TCP" -Direction Inbound -Program $UDKPath -Action Allow -Protocol TCP
+$Description = "UDK.exe allowed rule."
+
+New-NetFirewallRule -DisplayName "UDK.exe Outbound UDP" -Direction Outbound -Program $UDKPath `
+    -Action Allow -Protocol UDP -Profile Any -Description $Description -Enabled True
+
+New-NetFirewallRule -DisplayName "UDK.exe Outbound TCP" -Direction Outbound -Program $UDKPath `
+    -Action Allow -Protocol TCP -Profile Any -Description $Description -Enabled True
+
+New-NetFirewallRule -DisplayName "UDK.exe Inbound UDP" -Direction Inbound -Program $UDKPath `
+    -Action Allow -Protocol UDP -Profile Any -Description $Description -Enabled True
+
+New-NetFirewallRule -DisplayName "UDK.exe Inbound TCP" -Direction Inbound -Program $UDKPath `
+    -Action Allow -Protocol TCP -Profile Any -Description $Description -Enabled True
--- tests/UDKTests/run_udk.ps1
+++ tests/UDKTests/run_udk.ps1
@@ -1,1 +1,5 @@
-Start-Process -NoNewWindow -FilePath $PSScriptRoot\UDK-Lite\Binaries\Win64\UDK.com -ArgumentList "make","-useunpublished"
+Start-Process -NoNewWindow -FilePath $PSScriptRoot\UDK-Lite\Binaries\Win64\UDK.com -ArgumentList "make", "-useunpublished"
+
+# TODO: this can list all event logs for "UDK".
+Get-WinEvent -ListLog * |
+        ForEach-Object -parallel { Get-WinEvent @{ logname = $_.logname } -ea 0 } | Where-Object message -Match UDK
