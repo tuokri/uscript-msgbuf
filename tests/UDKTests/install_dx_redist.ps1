28e61c011ec39ffac80f125609920e5533202e24
Tuomo Kriikkula <tuokri@tuta.io>
2023-11-15 14:56:40 +0200

    Fix DirectX installer arguments

--- tests/UDKTests/install_dx_redist.ps1
+++ tests/UDKTests/install_dx_redist.ps1
@@ -12,4 +12,4 @@ Invoke-WebRequest `
     https://download.microsoft.com/download/8/4/A/84A35BF1-DAFE-4AE8-82AF-AD2AE20B6B14/directx_Jun2010_redist.exe `
     -OutFile $DX_REDIST_EXE
 
-& $DX_REDIST_EXE /silent
+& $DX_REDIST_EXE /Q
--- tests/UDKTests/install_dx_sdk.ps1
+++ tests/UDKTests/install_dx_sdk.ps1
@@ -4,4 +4,4 @@ Invoke-WebRequest `
     https://download.microsoft.com/download/A/E/7/AE743F1F-632B-4809-87A9-AA1BB3458E31/DXSDK_Jun10.exe `
     -OutFile $DX_SDK_EXE
 
-& $DX_SDK_EXE /silent
+& $DX_SDK_EXE /U
