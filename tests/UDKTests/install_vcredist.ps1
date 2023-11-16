7786bdc185b746ca15563a0db3864341c7290519
Tuomo <tuokri@users.noreply.github.com>
2023-11-16 16:37:28 +0200

    Use -Force when creating directory

--- tests/UDKTests/install_vcredist.ps1
+++ tests/UDKTests/install_vcredist.ps1
@@ -1,7 +1,7 @@
 $ErrorView = 'NormalView'
 
 Install-Module -Name VcRedist -Force
-New-Item -Path C:\Temp\VcRedist -ItemType Directory
+New-Item -Path C:\Temp\VcRedist -ItemType Directory -Force
 $VcList = Get-VcList -Release "2012"
 Save-VcRedist -VcList $VcList -Path C:\Temp\VcRedist
 Install-VcRedist -Path C:\Temp\VcRedist -VcList $VcList -Silent
